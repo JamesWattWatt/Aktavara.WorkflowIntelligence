5f71eecfe34dfcfd163830c1bd2855c9a6a00d6d
James Watt <james.watt@enghouse.com>
2026-06-16 11:12:40 +0200

    Prompt 25a verified - backfill working, LLM questions high quality

--- Aktavara.WorkflowIntelligence.Api/obj/Debug/net10.0/Aktavara.WorkflowIntelligence.Api.AssemblyInfo.cs
+++ Aktavara.WorkflowIntelligence.Api/obj/Debug/net10.0/Aktavara.WorkflowIntelligence.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aktavara.WorkflowIntelligence.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8411108e25fbc3da911e47f650e1478723e9ead5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7397af135089fa71bc4025dc2df0c7cbcd827c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aktavara.WorkflowIntelligence.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aktavara.WorkflowIntelligence.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
